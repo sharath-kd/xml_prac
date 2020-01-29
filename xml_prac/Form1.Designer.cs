5603614eb3a2b7b6117ae8db33670b4332ce9293
sharath-kd <43500846+sharath-kd@users.noreply.github.com>
2020-01-29 10:25:41 +0530

    Add files via upload

---
+++ xml_prac/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace xml_prac
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.display_textBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.update_textBox = new System.Windows.Forms.TextBox();
+            this.search_button = new System.Windows.Forms.Button();
+            this.update_button = new System.Windows.Forms.Button();
+            this.search_textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // display_textBox
+            // 
+            this.display_textBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.display_textBox.Location = new System.Drawing.Point(36, 84);
+            this.display_textBox.Multiline = true;
+            this.display_textBox.Name = "display_textBox";
+            this.display_textBox.Size = new System.Drawing.Size(256, 109);
+            this.display_textBox.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(193, 298);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Create XML file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(193, 327);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Display Contents";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // update_textBox
+            // 
+            this.update_textBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.update_textBox.Location = new System.Drawing.Point(36, 209);
+            this.update_textBox.Name = "update_textBox";
+            this.update_textBox.Size = new System.Drawing.Size(256, 20);
+            this.update_textBox.TabIndex = 3;
+            // 
+            // search_button
+            // 
+            this.search_button.Location = new System.Drawing.Point(193, 43);
+            this.search_button.Name = "search_button";
+            this.search_button.Size = new System.Drawing.Size(99, 23);
+            this.search_button.TabIndex = 4;
+            this.search_button.Text = "Search";
+            this.search_button.UseVisualStyleBackColor = true;
+            this.search_button.Click += new System.EventHandler(this.search_button_Click);
+            // 
+            // update_button
+            // 
+            this.update_button.Location = new System.Drawing.Point(193, 240);
+            this.update_button.Name = "update_button";
+            this.update_button.Size = new System.Drawing.Size(99, 23);
+            this.update_button.TabIndex = 5;
+            this.update_button.Text = "Update Value";
+            this.update_button.UseVisualStyleBackColor = true;
+            this.update_button.Click += new System.EventHandler(this.update_button_Click);
+            // 
+            // search_textBox
+            // 
+            this.search_textBox.Location = new System.Drawing.Point(36, 45);
+            this.search_textBox.Name = "search_textBox";
+            this.search_textBox.Size = new System.Drawing.Size(151, 20);
+            this.search_textBox.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 362);
+            this.Controls.Add(this.search_textBox);
+            this.Controls.Add(this.update_button);
+            this.Controls.Add(this.search_button);
+            this.Controls.Add(this.update_textBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.display_textBox);
+            this.Name = "Form1";
+            this.Text = "xml_eg";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox display_textBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox update_textBox;
+        private System.Windows.Forms.Button search_button;
+        private System.Windows.Forms.Button update_button;
+        private System.Windows.Forms.TextBox search_textBox;
+    }
+}
+
---
+++ xml_prac/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace xml_prac
+{
+    public partial class Form1 : Form
+    {
+        int[] temp = new int[2];
+        public Form1()
+        {
+            InitializeComponent();
+            display_textBox.ReadOnly = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            XmlTextWriter writer = new XmlTextWriter("product.xml", Encoding.UTF8);
+            writer.WriteStartDocument(true);
+            writer.Formatting = Formatting.Indented;
+            writer.Indentation = 2;
+            writer.WriteStartElement("Table");
+            createNode("1", "Product 1", "1000", writer);
+            createNode("2", "Product 2", "2000", writer);
+            createNode("3", "Product 3", "3000", writer);
+            createNode("4", "Product 4", "4000", writer);
+            writer.WriteEndElement();
+            writer.WriteEndDocument();
+            writer.Close();
+            MessageBox.Show("XML File created ! ");
+        }
+
+        private void createNode(string pID, string pName, string pPrice, XmlWriter writer)
+        {
+            writer.WriteStartElement("Product");
+            writer.WriteStartElement("Product_id");
+            writer.WriteString(pID);
+            writer.WriteEndElement();
+            writer.WriteStartElement("Product_name");
+            writer.WriteString(pName);
+            writer.WriteEndElement();
+            writer.WriteStartElement("Product_price");
+            writer.WriteString(pPrice);
+            writer.WriteEndElement();
+            writer.WriteEndElement();
+        }
+
+        [Obsolete]
+        private void button2_Click(object sender, EventArgs e)
+        {
+            XmlDataDocument xmldoc = new XmlDataDocument();
+            XmlNodeList xmlnode;
+            int i = 0, j = 0;
+            string str = null;
+            FileStream fs = new FileStream("product.xml", FileMode.Open, FileAccess.Read);
+            xmldoc.Load(fs);
+            xmlnode = xmldoc.GetElementsByTagName("Product");
+            for (i = 0; i <= xmlnode.Count - 1; i++)
+            {
+                xmlnode[i].ChildNodes.Item(0).InnerText.Trim();
+                str = Environment.NewLine;
+                for ( j = 0; j < xmlnode[i].ChildNodes.Count; j++)
+                {
+                    str += xmlnode[i].ChildNodes.Item(j).InnerText.Trim() + "  ";
+                }
+                display_textBox.Text += str;
+            }
+            /*fs.Close();*/
+        }
+
+        [Obsolete]
+        private void search_button_Click(object sender, EventArgs e)
+        {
+            XmlDataDocument xmldoc = new XmlDataDocument();
+            XmlNodeList xmlnode;
+            int i = 0, j = 0;
+            string str = null;
+            FileStream fs = new FileStream("product.xml", FileMode.Open, FileAccess.Read);
+            xmldoc.Load(fs);
+            xmlnode = xmldoc.GetElementsByTagName("Product");
+            for (j = 0; j < xmlnode.Count; j++)
+            {
+                if (search_textBox.Text== xmlnode[j].ChildNodes.Item(0).InnerText.Trim().ToString())
+                {
+                    for (i = 1; i < xmlnode[j].ChildNodes.Count; i++)
+                    {
+                        str += xmlnode[j].ChildNodes.Item(i).InnerText.Trim() + "  ";
+                    }
+                    temp[0] = j;
+                    temp[1] = i;
+                }
+                display_textBox.Text = str;
+            }
+            fs.Close();
+        }
+
+        [Obsolete]
+        private void update_button_Click(object sender, EventArgs e)
+        {
+            XmlDataDocument xmldoc = new XmlDataDocument();
+            XmlNodeList xmlnode;
+            int i = temp[0], j = temp[1];
+            string str = null;
+            FileStream fs = new FileStream("product.xml", FileMode.Open, FileAccess.ReadWrite);
+            xmldoc.Load(fs);
+            XmlElement root = xmldoc.DocumentElement;
+            XmlNodeList nodes = root.SelectNodes("/Table/Product");
+            foreach (XmlNode node in nodes)
+            {
+                if (node["Product_id"].InnerText.ToString().Trim() == search_textBox.Text)
+                {
+                    node["Product_price"].InnerText = update_textBox.Text;
+                }
+            }
+            xmldoc.Save(fs);
+            /*
+            xmlnode = xmldoc.GetElementsByTagName("Product");
+            xmlnode[i].ChildNodes.Item(j).InnerText.Trim();
+            str = update_textBox.Text;
+            xmlnode[i].ChildNodes.Item(j).InnerText = str;*/
+        }
+    }
+}
